b17d9f71da73f3fde70e9f6725d54d161d18e25c
liuxiao <liuxiao@longshine.com>
2021-01-28 17:03:52 +0800

    juc sleep yield join

---
+++ src/main/java/com/lx/juc/c_0000/T03_Sleep_Yield_Join.java
@@ -0,0 +1,83 @@
+package com.lx.juc.c_0000;
+
+import com.sun.org.glassfish.external.statistics.Statistic;
+
+public class T03_Sleep_Yield_Join {
+    public static void main(String[] args) {
+//        testSleep();
+//        testYield();
+        testJoin();
+    }
+
+    static void testSleep(){
+        new Thread(() -> {
+            for(int i=0; i<100; i++){
+                System.out.println("A:" + i);
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    static void testYield(){
+        new Thread(() -> {
+            for(int i=0; i< 100; i++){
+                System.out.println("A:" + i);
+                if(i%10 == 0){
+                    Thread.yield();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for(int i=0; i< 100; i++){
+                System.out.println("-------------------------------B:" + i);
+                if(i%10 == 0){
+                    Thread.yield();
+                }
+            }
+        }).start();
+    }
+
+
+    static void testJoin(){
+        Thread t1 = new Thread(() -> {
+            for(int i=0; i< 100; i++){
+                System.out.println("A:" + i);
+
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            for(int i=0; i< 100; i++){
+                System.out.println("-------------B:" + i);
+
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
+
+
