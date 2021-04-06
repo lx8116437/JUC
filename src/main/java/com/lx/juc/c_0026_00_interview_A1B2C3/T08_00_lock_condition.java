809385dc1ee9c6515f5ffc4d4d7a6826c1a28018
liuxiao <956788625@qq.com>
2021-04-07 06:59:15 +0800

    1A2B面试题

---
+++ src/main/java/com/lx/juc/c_0026_00_interview_A1B2C3/T08_00_lock_condition.java
@@ -0,0 +1,59 @@
+package com.lx.juc.c_0026_00_interview_A1B2C3;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class T08_00_lock_condition {
+
+    public static void main(String[] args) {
+        char[] a = "123456".toCharArray();
+        char[] b = "ABCDEF".toCharArray();
+
+
+        Lock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+
+        new Thread(() -> {
+            try {
+                lock.lock();
+                for (char c : a) {
+                    System.out.print(c);
+                    condition.signal();
+                    condition.await();
+                }
+                condition.signal();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                lock.unlock();
+            }
+        }, "t1").start();
+
+
+        new Thread(() -> {
+            try {
+                lock.lock();
+
+                for (char c : b){
+                    System.out.print(c);
+                    condition.signal();
+
+                    condition.await();
+                }
+            condition.signal();
+            }catch (Exception e){
+                e.printStackTrace();
+            }finally {
+                lock.unlock();
+            }
+        }, "t2").start();
+
+
+
+
+
+    }
+
+
+}
